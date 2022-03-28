d2ce4c8f27bcdf80a7393e77a0afa0fe2cd03def
Matth998 <matheusoliveira.3125@gmail.com>
2022-03-28 18:34:14 -0300

    Criação dos componentes: Menu, Rodapé, Login, Cadastro e Home.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+  {path: '', redirectTo: 'home',pathMatch:'full'},
+
+  {path:'home', component:HomeComponent}, 
+  {path:'login', component:LoginComponent},
+  {path:'register', component:RegisterComponent}
+  
+]
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
