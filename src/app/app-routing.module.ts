1aa1718c0637f162c445d1e9fba60aa9cef0ce55
larissadecampos2@gmail.com <larissadecampos2@gmail.com>
2022-04-01 14:59:26 -0300

    Login and Feed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeedComponent } from './feed/feed.component';
 
 const routes: Routes = [
 
@@ -10,7 +11,8 @@ const routes: Routes = [
 
   {path:'home', component:HomeComponent}, 
   {path:'login', component:LoginComponent},
-  {path:'register', component:RegisterComponent}
+  {path:'register', component:RegisterComponent},
+  {path:'feed', component:FeedComponent}
   
 ]
 @NgModule({
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UserLogin } from '../Model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin = new UserLogin();
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(){
+    window.scroll(0,0);
   }
+  login(){
+    this.authService.Login(this.userLogin).subscribe((resp:UserLogin) =>{
 
+      this.userLogin=resp;
+
+      environment.token=this.userLogin.token;
+      environment.id=this.userLogin.id;
+      environment.name=this.userLogin.name;
+      environment.photo=this.userLogin.photo;
+
+      this.router.navigate(['/feed']);
+      
+    }, error =>{if(error.status==500 || error.status==401){
+
+      alert("Usuária ou senha inválidas!");
+
+    }})
+  }
 }
